f779a036c661019e8605d1d2de609517be6dfff8
Ulpio <ulpionetto0@gmail.com>
2024-06-13 21:29:19 -0300

    Finalizado - porem nao atualiza dados

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -41,3 +41,40 @@ func Insert(w http.ResponseWriter,r *http.Request){
 	}
 	http.Redirect(w,r,"/",301)
 }
+
+func Delete(w http.ResponseWriter,r *http.Request){
+	idProduto := r.URL.Query().Get("id")
+	models.DeleteProd(idProduto)
+	http.Redirect(w,r,"/",301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idDoProduto := r.URL.Query().Get("id")
+	produto := models.EditaProduto(idDoProduto)
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+			id := r.FormValue("id")
+			nome := r.FormValue("nome")
+			descricao := r.FormValue("descricao")
+			preco := r.FormValue("preco")
+			quantidade := r.FormValue("quantidade")
+
+			idConvertido, err := strconv.Atoi(id)
+
+			precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+			if err != nil {
+					log.Println("Erro na convesão do preço para float64:", err)
+			}
+
+			quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+			if err != nil {
+					log.Println("Erro na convesão da quantidade para int:", err)
+			}
+
+			models.AtualizaProduto(idConvertido, nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+	}
+	http.Redirect(w, r, "/", 301)
+}
